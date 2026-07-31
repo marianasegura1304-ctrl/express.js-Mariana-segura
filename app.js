824c9b103477ee498a8ce1db8fb88c3aceb79e47
Adriana-Buitrago <adrianaluci2810@gmail.com>
2026-07-31 14:07:22 -0500

    dependencias dorenv, forma 1

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 import express from 'express';
-  const app = express(); 
-   const port = 3000; 
+import {configDotenv} from "dotenv"
+  configDotenv()
+   const port = process.env.PUERTO || 3000; 
    app.get("/", (_, res) => {
-     res.send('Aprendicez ficha 3407186'); }); 
+     res.send('Aprendicez ficha 3407186'); });
+
      app.listen(port, () => { 
-        console.log( `Servidor en funcionamiento en el puerto: ${port}`); }); 
+        console.log( `SERVIDOR: https://localhost: ${port}`); }); 
